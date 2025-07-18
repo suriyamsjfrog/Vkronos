86d756430c7accd7aea8ac07a9a7c219606001f8
Sushmita1999 <60175936+Sushmita1999@users.noreply.github.com>
2025-07-18 11:38:11 +0530

    Update index.js
    test

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get("/", async (req, res) => {
       });
     }
   } else {
-    console.log("Hi there, this is a test development test for frogbot 4 ");
+    console.log("Hi there, this is a test development test for frogbot 5 ");
     res.render("signup_sign", {
       error: ""
     });
